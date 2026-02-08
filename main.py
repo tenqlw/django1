9f1795e64cdde5ba28c157e3fd31be2a28725f44
Alisa Choi <cojalisa072@gmail.com>
2026-02-09 02:15:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ from django.db import models
 from datacenter.models import Passcard 
 
 if __name__ == '__main__':
-    posts = Passcard.objects.all()
     active_posts = Passcard.objects.filter(is_active=True)
     print('Всего пропусков:', Passcard.objects.count())
     print('Активных пропусков', len(active_posts))
